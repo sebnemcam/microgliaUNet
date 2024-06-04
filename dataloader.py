3b8310545fd7920736ecb5ea5fc87a3b5a04e7dd
Şebnem Çam <sebnemacam@gmail.com>
2024-06-04 16:59:41 +0200

    train & val step

--- dataloader.py
+++ dataloader.py
@@ -1,6 +1,8 @@
 import logging
 import os
 import time
+
+from monai.inferers import sliding_window_inference
 from tqdm import tqdm
 
 import numpy as np
@@ -10,7 +12,7 @@ import torch
 
 import monai
 from monai.utils import first
-from monai.data import ArrayDataset, DataLoader, partition_dataset
+from monai.data import ArrayDataset, DataLoader, partition_dataset, decollate_batch
 from monai.networks.nets import Unet
 from monai.networks.layers import Norm
 from monai.losses import DiceLoss
@@ -24,6 +26,7 @@ from monai.transforms import EnsureChannelFirst, Compose, RandRotate90, Resize, 
 
 path_seg = "/Users/sebnemcam/Desktop/microglia/input cxc31/gt_new/"
 path_img = "/Users/sebnemcam/Desktop/microglia/input cxc31/raw_new/"
+directory = "/Users/sebnemcam/Desktop/Helmholtz/"
 
 seg_list = os.listdir(path_seg)
 img_list = os.listdir(path_img)
@@ -34,12 +37,10 @@ for i in range(len(seg_list)):
     if "nii.gz" in seg_list[i] and img_list[i] == seg_list[i]:
 
         segFile = os.path.join(path_seg, seg_list[i])
-        #segmentations.append(nib.load(segFile))
         segmentations.append(segFile)
         #print(segmentations)
 
         imgFile = os.path.join(path_img, img_list[i])
-        #images.append(nib.load(imgFile))
         images.append(imgFile)
         #print(images)
 
@@ -49,7 +50,8 @@ img_transforms = Compose(
     [
         LoadImage(image_only=True),
         EnsureChannelFirst(),
-        ScaleIntensity()
+        ScaleIntensity(),
+        Resize((128,128,128))
     ]
 )
 
@@ -57,6 +59,7 @@ basic_transforms = Compose(
     [
         LoadImage(image_only=True),
         EnsureChannelFirst(),
+        Resize((128,128,128))
     ]
 )
 
@@ -76,8 +79,6 @@ train_images = [train_files_a[i][0] for i in range(int(ratio_b))]
 train_segmentations = [train_files_a[i][1] for i in range(int(ratio_b))]
 val_images = [train_files_a[i][0] for i in range(int(ratio_b),len(train_files_a))]
 val_segmentations = [train_files_a[i][1] for i in range(int(ratio_b),len(train_files_a))]
-
-
 
 '''
 print(f"Train length: {len(train_images)}")
@@ -99,9 +100,10 @@ val_data = ArrayDataset(val_images,basic_transforms,val_segmentations,basic_tran
 #visualizing some slices from the training data to make sure everything is fine
 im, seg = train_data[0]
 num_slices = im.shape[1]
+print(im.shape)
 
-num_slices = im.shape[1]  # Assuming im has shape [1, slices, height, width]
-num_rows = (num_slices // 10)  # Number of rows for up to 10 slices per row
+num_slices = im.shape[1]
+num_rows = (num_slices // 10)
 fig, axes = plt.subplots(num_rows, 2, figsize=(12, 6 * num_rows))
 
 for idx, slice_idx in enumerate(range(0, num_slices, max(1, num_slices // 10))):  # Sample up to 10 slices
@@ -126,30 +128,17 @@ print(f"val_data, length: {len(val_data)}")
 '''
 
 #build Dataloader
-train_loader = DataLoader(train_data, batch_size=5)
+train_loader = DataLoader(train_data, batch_size=1)
 train_batch = first(train_loader)
-#print(batch[0].shape)
+print(train_batch[0].shape)
+#print(torch.reshape(train_batch[0],(1,1,128,128,128)))
 
 test_loader = DataLoader(test_data)
 val_loader = DataLoader(val_data)
 val_batch = first(val_loader)
 #print(f"val_batch: {val_batch[1].shape}")
 
-'''
-fig, ax = plt.subplots(2, 1, figsize=(8, 4))
-
-print("Visualizing <3")
-
-ax[0].imshow(np.hstack(train_batch[0][:, 0, 80]),cmap='gray')
-ax[0].set_title("Batch of Images")
-
-ax[1].imshow(np.hstack(train_batch[1][:, 0, 80]),cmap='gray')
-ax[1].set_title("Batch of Segmentations")
-
-plt.tight_layout()
-plt.show()
-#fig.savefig("/lustre/groups/iterm/sebnem/slurm_outputs/batches.png")
-'''
+print("Checkpoint 1")
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -161,19 +150,24 @@ model = Unet(
     out_channels=1,
     channels=(16, 32, 64, 128, 256),
     strides=(2,2,2,2),
-    norm=Norm.BATCH,
-    #act=torch.nn.functional.sigmoid()
+    num_res_units=2,
+    norm=Norm.BATCH
 ).to(device)
+
+print("Checkpoint 2")
 
 loss_function = DiceLoss(sigmoid=True)
 optimizer = torch.optim.Adam(model.parameters(), 1e-4)
 dice_metric = DiceMetric()
+metric_values = []
 
 epoch_loss_values = []
 
 max_epochs = 200
-val_interval = max_epochs/4
+val_interval = 2
+best_metric = -1
 
+print("Checkpoint 3")
 for epoch in range(1, max_epochs):
     print("-" * 10)
     print(f"epoch {epoch}/{max_epochs}")
@@ -182,13 +176,16 @@ for epoch in range(1, max_epochs):
     step = 0
     for batch_data in tqdm(train_loader):
         step += 1
-        imgs, segs = (
+        img, seg = (
             batch_data[0].to(device),
             batch_data[1].to(device),
         )
+        print(f"Input to model: {img.size()}")
+        print(f"Segmentations: {seg.size()}")
         optimizer.zero_grad()
-        outputs = model(imgs)
-        loss = loss_function(outputs, segs)
+        outputs = model(img)
+        print(f"Output from model: {outputs.shape}")
+        loss = loss_function(outputs, seg)
         loss.backward()
         optimizer.step()
         epoch_loss += loss.item()
@@ -208,13 +205,38 @@ for epoch in range(1, max_epochs):
                     val_data[0].to(device),
                     val_data[1].to(device)
                 )
+                roi_size = (160, 160, 160)
+                sw_batch_size = 4
+                val_outputs = sliding_window_inference(
+                    val_img, roi_size, sw_batch_size, model)
+                val_outputs = [i for i in decollate_batch(val_outputs)]
+                val_seg = [i for i in decollate_batch(val_seg)]
+                # compute metric for current iteration
+                dice_metric(y_pred=val_outputs, y=val_seg)
+
+                # aggregate the final mean dice result
+                metric = dice_metric.aggregate().item()
+                # reset the status for next validation round
+                dice_metric.reset()
+
+                metric_values.append(metric)
+                if metric > best_metric:
+                    best_metric = metric
+                    best_metric_epoch = epoch + 1
+                    torch.save(model.state_dict(), os.path.join(
+                        directory, "best_metric_model.pth"))
+                    print("saved new best metric model")
+                print(
+                    f"current epoch: {epoch + 1} current mean dice: {metric:.4f}"
+                    f"\nbest mean dice: {best_metric:.4f} "
+                    f"at epoch: {best_metric_epoch}"
+                )
 
 
 
 '''
 STILL TO DO
     - Figure out which transforms to use
-    - train step and validation
     - track metrics
     - 5-fold cross Validation
     - testing 
