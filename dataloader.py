3cc736acd224ee34b9885e8364e3ba5801945fdc
Şebnem Çam <sebnemacam@gmail.com>
2024-06-13 16:37:06 +0200

    bug fixes

--- dataloader.py
+++ dataloader.py
@@ -10,16 +10,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import nibabel as nib
 import torch
+import torchmetrics
 
 import monai
 from monai.utils import first
-from monai.data import ArrayDataset, DataLoader, partition_dataset, decollate_batch, Dataset
+from monai.data import ArrayDataset, DataLoader, partition_dataset, decollate_batch, Dataset, CacheDataset
 from monai.networks.nets import Unet
 from monai.networks.layers import Norm
 from monai.losses import DiceLoss
 from monai.metrics import DiceMetric
-from monai.transforms import EnsureChannelFirst, Compose, RandRotate90, Resize, ScaleIntensity, LoadImage, \
-    EnsureChannelFirstd, LoadImaged, Resized
+from monai.transforms import EnsureChannelFirst, Compose, RandRotate90, Resize, ScaleIntensity, LoadImage, LoadImaged, Resized, ToTensord, RandFlipd
 
 #!!!!!!PATH NEEDS TO BE ADJUSTED FOR HPC !!!!!!!!!
 
@@ -43,93 +43,64 @@ for i in range(len(seg_list)):
 
         segFile = os.path.join(path_seg, seg_list[i])
         segmentations.append(segFile)
-        #print(segmentations)
 
         imgFile = os.path.join(path_img, img_list[i])
         images.append(imgFile)
-        #print(images)
 
-#which transforms should be applied????, croping & flipping seems countereffective
-#should transforms only be applied to train set?
-img_transforms = Compose(
-    [
-        LoadImage(image_only=True),
-        EnsureChannelFirst(),
-        ScaleIntensity(),
-        Resize((128,128,128))
-    ]
-)
-
-basic_transforms = Compose(
-    [
-        LoadImage(image_only=True),
-        EnsureChannelFirst(),
-        Resize((128,128,128))
-    ]
-)
-
-dic_trasforms = Compose(
-    [
-        LoadImaged(keys=['image','segmentation'],image_only=True),
-        EnsureChannelFirstd(keys=['image','segmentation']),
-        Resized(keys=['image','segmentation'],spatial_size=(128, 128, 128))
-    ]
-)
 
 # zip data into one list of image/segmenttaion pairs
-# (not exactly necessary but I get paranoid about accidentally messing up the sequences)
 zipped_data = list(zip(images,segmentations))
 #print(f"Zip length: {len(zipped_data)}")
 
 #split into train, test & validation files
 ratio_a = len(zipped_data)*0.8
 train_files_a = [zipped_data[i] for i in range(int(ratio_a))]
-test_images = [zipped_data[i][0] for i in range(int(ratio_a),len(zipped_data))]
-test_segmentations = [zipped_data[i][1] for i in range(int(ratio_a),len(zipped_data))]
-
-ratio_b = len(train_files_a)*0.8
-train_images = [train_files_a[i][0] for i in range(int(ratio_b))]
-train_segmentations = [train_files_a[i][1] for i in range(int(ratio_b))]
-val_images = [train_files_a[i][0] for i in range(int(ratio_b),len(train_files_a))]
-val_segmentations = [train_files_a[i][1] for i in range(int(ratio_b),len(train_files_a))]
-
-'''
 test_files = [zipped_data[i] for i in range(int(ratio_a),len(zipped_data))]
+ratio_b = len(train_files_a)*0.8
 train_files = [train_files_a[i] for i in range(int(ratio_b))]
 val_files = [train_files_a[i] for i in range(int(ratio_b),len(train_files_a))]
 
-train_set = Dataset(train_files,img_transforms)
-test_set = Dataset(test_files,img_transforms)
-val_set = Dataset(val_files,img_transforms)
-'''
-
+train_data = [{'image' : img, 'segmentation' : seg } for img,seg in train_files]
+val_data = [{'image' : img, 'segmentation' : seg } for img,seg in val_files]
+test_data = [{'image' : img, 'segmentation' : seg } for img,seg in test_files]
 
 '''
-print(f"Train length: {len(train_images)}")
-print(f"Test length: {len(test_images)}")
-print(f"Val length: {len(val_images)}")
-print(f"Train Files: {train_images}")
-print(f"Test Files: {test_images}")
-print(f"Val images: {val_images}")
-print(f"Val segs: {val_segmentations}")
-print(f"images: {images}")
-print(f"segs: {segmentations}")
+print(f"Len Train Data: {len(train_data)}")
+print(f"Len Val Data: {len(val_data)}")
+print(f"Len Test Data: {len(test_data)}")
 '''
-#create datasets from the files with respective transforms
-train_data = ArrayDataset(train_images, img_transforms,train_segmentations,basic_transforms)
-test_data = ArrayDataset(test_images,basic_transforms,test_segmentations,basic_transforms)
-val_data = ArrayDataset(val_images,basic_transforms,val_segmentations,basic_transforms)
 
+keys = ['image','segmentation']
+
+dic_transforms_train = Compose(
+    [
+        LoadImaged(keys,image_only=True,ensure_channel_first=True),
+        Resized(keys,spatial_size=(128, 128, 128)),
+        ToTensord(keys),
+        RandFlipd(keys, spatial_axis=0, prob=0.5),
+        RandFlipd(keys, spatial_axis=1, prob=0.5),
+        RandFlipd(keys, spatial_axis=2, prob=0.5),
+
+    ]
+)
+
+dic_transforms = Compose(
+    [
+        LoadImaged(keys, ensure_channel_first=True, image_only=True),
+        ToTensord(keys)
+    ]
+)
+
+train_set = CacheDataset(train_data,dic_transforms_train)
+test_set = CacheDataset(test_data,dic_transforms)
+val_set = CacheDataset(val_data,dic_transforms_train)
 
 #visualizing some slices from the training data to make sure everything is fine
-#train_data = Dataset(train_files_a,img_transforms)
-im, seg = train_data[0]
-num_slices = im.shape[1]
-#print(im.shape)
-
+sample = train_set[0]
+im,seg = sample['image'], sample['segmentation']
 num_slices = im.shape[1]
 num_rows = (num_slices // 10)
-fig, axes = plt.subplots(num_rows, 2, figsize=(12, 6 * num_rows))
+fig, axes = plt.subplots(num_rows-1, 2, figsize=(12, 6 * num_rows))
 for idx, slice_idx in enumerate(range(0, num_slices, max(1, num_slices // 10))):  # Sample up to 10 slices
     row = idx
     # Image slice subplot
@@ -151,22 +122,10 @@ print(f"train_data, length: {len(train_data)}")
 print(f"val_data, length: {len(val_data)}")
 '''
 
-#train_loader = DataLoader(train_set, batch_size=1)
-#test_loader = DataLoader(test_set,batch_size=1)
-#val_loader = DataLoader(val_set,batch_size=1)
-
 #build Dataloader
-train_loader = DataLoader(train_data, batch_size=1)
-train_batch = first(train_loader)
-#print(train_batch[0].shape)
-#print(torch.reshape(train_batch[0],(1,1,128,128,128)))
-
-test_loader = DataLoader(test_data)
-val_loader = DataLoader(val_data)
-val_batch = first(val_loader)
-#print(f"val_batch: {val_batch[1].shape}")
-train_batch = first(train_loader)
-#print(train_batch[0].shape)
+train_loader = DataLoader(train_set,batch_size=5)
+test_loader = DataLoader(test_set,batch_size=5)
+val_loader = DataLoader(val_set,batch_size=5)
 
 print("Checkpoint 1")
 
@@ -188,7 +147,7 @@ print("Checkpoint 2")
 
 loss_function = DiceLoss(sigmoid=True)
 optimizer = torch.optim.Adam(model.parameters(), 1e-4)
-dice_metric = DiceMetric()
+dice_metric = torchmetrics.Dice(zero_division=1)
 metric_values = []
 
 epoch_loss_values = []
@@ -204,11 +163,11 @@ for epoch in range(1, max_epochs):
     model.train()
     epoch_loss = 0
     step = 0
-    for batch_data in test_loader:
+    for batch_data in train_loader:
         step += 1
         img, seg = (
-            batch_data[0].to(device),
-            batch_data[1].to(device),
+            batch_data['image'].to(device),
+            batch_data['segmentation'].to(device),
         )
         #print(f"Input to model: {img.size()}")
         #print(f"Segmentations: {seg.size()}")
@@ -233,8 +192,8 @@ for epoch in range(1, max_epochs):
         with torch.no_grad():
             for val_data in val_loader:
                 val_img, val_seg = (
-                    val_data[0].to(device),
-                    val_data[1].to(device)
+                    val_data['image'].to(device),
+                    val_data['segmentation'].to(device)
                 )
                 roi_size = (160, 160, 160)
                 sw_batch_size = 4
@@ -243,7 +202,8 @@ for epoch in range(1, max_epochs):
                 val_outputs = [i for i in decollate_batch(val_outputs)]
                 val_seg = [i for i in decollate_batch(val_seg)]
                 # compute metric for current iteration
-                dice_metric(y_pred=val_outputs, y=val_seg)
+                dice_metric(val_outputs, val_seg)
+                print(f"output: {val_outputs}/n label: {val_seg}")
 
                 # aggregate the final mean dice result
                 metric = dice_metric.aggregate().item()
@@ -265,13 +225,13 @@ for epoch in range(1, max_epochs):
                     )
 
 
-checkpoint = {
+checkpoint =pd.DataFrame( {
 
      'train loss': epoch_loss_values,
      'dice values': metric_values,
      'best metric epoch': best_metric_epoch,
      'best metric': best_metric,
-}
+})
 
 
 val_interval = 2
@@ -306,7 +266,6 @@ plt.savefig("/lustre/groups/iterm/sebnem/LearningCurves.png")
 
 '''
 STILL TO DO
-    - Figure out which transforms to use
     - track metrics
     - 5-fold cross Validation
 '''
