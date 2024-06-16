420df1789ba89930855af9528b1897adce003081
Şebnem Çam <sebnemacam@gmail.com>
2024-06-16 13:03:15 +0200

    lrs

--- dataloader.py
+++ dataloader.py
@@ -147,143 +147,118 @@ model = Unet(
 
 print("Checkpoint 2")
 
-loss_function = DiceLoss(sigmoid=True)
-optimizer = torch.optim.Adam(model.parameters(), 1e-4)
+learning_rates = [1e-2,1e-3,1e-4,1e-5]
+loss_function = DiceLoss()
 dice_metric = torchmetrics.Dice(zero_division=1)
 dice_metric = DiceMetric(ignore_empty=False)
 metric_values = []
 
 epoch_loss_values = []
 
-max_epochs = 1000
+max_epochs = 1500
 val_interval = 1
 best_metric = -1
 post_pred = AsDiscrete(argmax=True)
 post_label=AsDiscrete()
 
 print("Checkpoint 3")
-for epoch in range(0, max_epochs):
-    print("-" * 10)
-    print(f"epoch {epoch+1}/{max_epochs}")
-    model.train()
-    epoch_loss = 0
-    step = 0
-    for batch_data in test_loader:
-        step += 1
-        img, seg = (
-            batch_data['image'].to(device),
-            batch_data['segmentation'].to(device),
-        )
-        #print(f"Input to model: {img.size()}")
-        #print(f"Segmentations: {seg.size()}")
-        optimizer.zero_grad()
-        outputs = model(img)
-        #print(f"Output from model: {outputs.shape}")
-        loss = loss_function(outputs, seg)
-        loss.backward()
-        optimizer.step()
-        epoch_loss += loss.item()
-        print(
-             f"{step}/{len(train_data) // train_loader.batch_size}, "
-             f"train loss: {loss.item():.4f}")
 
-    epoch_loss /= step
-    epoch_loss_values.append(epoch_loss)
-    print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
+fig, axs = plt.subplots(1, 2, figsize=(18, 7))
 
-    if epoch % val_interval == 0:
-        print("Validation")
-        model.eval()
-        with torch.no_grad():
-            for val_data in val_loader:
-                val_img, val_seg = (
-                    val_data['image'].to(device),
-                    val_data['segmentation'].to(device)
-                )
-                roi_size = (160, 160, 160)
-                sw_batch_size = 4
-                val_outputs = sliding_window_inference(
-                    val_img, roi_size, sw_batch_size, model)
-                val_outputs = [post_pred(i) for i in decollate_batch(val_outputs)]
-                val_seg = [post_label(i) for i in decollate_batch(val_seg)]
-                # compute metric for current iteration
-                dice_metric(y_pred=val_outputs, y=val_seg)
-                #print(f"output: {val_outputs}/n label: {val_seg}")
+for lr in learning_rates:
+    optimizer = torch.optim.Adam(model.parameters(),lr)
+    for epoch in range(0, max_epochs):
+        print(f"TRAINING WITH LEARNING RATE {lr}")
+        print("-" * 10)
+        print(f"epoch {epoch + 1}/{max_epochs}")
+        model.train()
+        epoch_loss = 0
+        step = 0
+        for batch_data in test_loader:
+            step += 1
+            img, seg = (
+                batch_data['image'].to(device),
+                batch_data['segmentation'].to(device),
+            )
+            # print(f"Input to model: {img.size()}")
+            # print(f"Segmentations: {seg.size()}")
+            optimizer.zero_grad()
+            outputs = model(img)
+            # print(f"Output from model: {outputs.shape}")
+            loss = loss_function(outputs, seg)
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+            print(
+                f"{step}/{len(train_data) // train_loader.batch_size}, "
+                f"train loss: {loss.item():.4f}")
 
-                # aggregate the final mean dice result
-            metric = dice_metric.aggregate().item()
+        epoch_loss /= step
+        epoch_loss_values.append(epoch_loss)
+        print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
+
+        if epoch % val_interval == 0:
+            print("Validation")
+            model.eval()
+            with torch.no_grad():
+                for val_data in val_loader:
+                    val_img, val_seg = (
+                        val_data['image'].to(device),
+                        val_data['segmentation'].to(device)
+                    )
+                    roi_size = (160, 160, 160)
+                    sw_batch_size = 4
+                    val_outputs = sliding_window_inference(
+                        val_img, roi_size, sw_batch_size, model)
+                    val_outputs = [post_pred(i) for i in decollate_batch(val_outputs)]
+                    val_seg = [post_label(i) for i in decollate_batch(val_seg)]
+                    # compute metric for current iteration
+                    dice_metric(y_pred=val_outputs, y=val_seg)
+                    # print(f"output: {val_outputs}/n label: {val_seg}")
+
+                    # aggregate the final mean dice result
+                metric = dice_metric.aggregate().item()
                 # reset the status for next validation round
-            dice_metric.reset()
-            metric_values.append(metric)
-            print(f"Dice Value: {metric}")
+                dice_metric.reset()
+                metric_values.append(metric)
+                print(f"Dice Value: {metric}")
 
-            if metric > best_metric:
-                best_metric = metric
-                best_metric_epoch = epoch + 1
-                torch.save(model.state_dict(), os.path.join(
-                            directory, "best_metric_model.pth"))
-                print("saved new best metric model")
-                print(
-                    f"current epoch: {epoch + 1} current mean dice: {metric:.4f}"
-                    f"\nbest mean dice: {best_metric:.4f} "
-                    f"at epoch: {best_metric_epoch}"
-                )
+                if metric > best_metric:
+                    best_metric = metric
+                    best_metric_epoch = epoch + 1
+                    torch.save(model.state_dict(), os.path.join(
+                        directory, "best_metric_model.pth"))
+                    print("saved new best metric model")
+                    print(
+                        f"current epoch: {epoch + 1} current mean dice: {metric:.4f}"
+                        f"\nbest mean dice: {best_metric:.4f} "
+                        f"at epoch: {best_metric_epoch}"
+                    )
 
+    # Plotting the loss and dice scores
+    axs[0].plot(range(1, len(epoch_loss_values) + 1), epoch_loss_values, label=f'LR={lr}')
+    axs[1].plot(range(1, len(metric_values) + 1), metric_values, label=f'LR={lr}')
+    epoch_loss_values = []
+    metric_values = []
 
+axs[0].set_xlabel('Epochs')
+axs[0].set_ylabel('Loss')
+axs[0].set_title('Dice Loss for Different Learning Rates')
+axs[0].legend()
 
-'''checkpoint =pd.DataFrame( {
-
-     'train_loss': epoch_loss_values,
-     'val_dice': metric_values,
-     'best_metric_epoch': best_metric_epoch,
-     'best_metric':best_metric
-})
-
-
-
-val_interval = 2
-plt.figure("train", (15, 5))
-plt.subplot(1, 2, 1)
-plt.title("Epoch Average Dice Loss")
-x = [i + 1 for i in range(len(checkpoint["train_loss"]))]
-y = checkpoint["train_loss"]
-plt.xlabel("#Epochs")
-plt.ylabel("Dice Loss")
-plt.plot(x, y)
-plt.plot(checkpoint["best_metric_epoch"],
-checkpoint["train_loss"][checkpoint["best_metric_epoch"]], 'r*', markersize=8)
-plt.subplot(1, 2, 2)
-plt.title("Val Mean Dice Score")
-x = [val_interval * (i + 1) for i in range(len(checkpoint["val_dice"]))]
-y = checkpoint["val_dice"]
-plt.xlabel("#Epochs")
-plt.plot(x, y)
-plt.plot(checkpoint["best_metric_epoch"],
-checkpoint["val_dice"][checkpoint["best_metric_epoch"]//2], 'r*', markersize=10)
-plt.annotate("Best Score[470, 0.9516]", xy=(checkpoint["best_metric_epoch"],
-checkpoint["val_dice"][checkpoint["best_metric_epoch"]//2]))'''
-
-# Plotting the loss and dice scores
-plt.figure(figsize=(12, 6))
-plt.subplot(1, 2, 1)
-plt.plot(range(1, len(epoch_loss_values) + 1), epoch_loss_values)
-plt.title("Epoch Loss")
-plt.xlabel("Epochs")
-plt.ylabel("Loss")
-
-plt.subplot(1, 2, 2)
-plt.plot(range(1, len(metric_values) + 1), metric_values)
-plt.title("Dice Metric")
-plt.xlabel("Epochs")
-plt.ylabel("Dice Score")
+axs[1].set_xlabel('Epochs')
+axs[1].set_ylabel('Dice Score')
+axs[1].set_title('Dice Score for Different Learning Rates')
+axs[1].legend()
 
 plt.show()
-plt.savefig("/lustre/groups/iterm/sebnem/LearningCurves1000.png")
-#plt.savefig("/Users/sebnemcam/Desktop/LearningCurves.png")
+plt.savefig("/lustre/groups/iterm/sebnem/LearningCurves_lr.png")
+# plt.savefig("/Users/sebnemcam/Desktop/LearningCurves.png")
+
+
 
 
 '''
 STILL TO DO
-    - track metrics
     - 5-fold cross Validation
 '''
