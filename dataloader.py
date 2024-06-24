9f48e3d645b6b15e6af7b758e89c9b6aac3e0d95
Şebnem Çam <sebnemacam@gmail.com>
2024-06-24 13:17:59 +0200

    lr schedule plot

--- dataloader.py
+++ dataloader.py
@@ -29,11 +29,11 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 path_seg = "/lustre/groups/iterm/Annotated_Datasets/Annotated Datasets/Microglia - Microglia LSM and Confocal/input cxc31/gt_new"
 path_img = "/lustre/groups/iterm/Annotated_Datasets/Annotated Datasets/Microglia - Microglia LSM and Confocal/input cxc31/raw_new"
 directory= "/lustre/groups/iterm/sebnem/"
-
-#path_seg = "/Users/sebnemcam/Desktop/microglia/input cxc31/gt_new/"
-#path_img = "/Users/sebnemcam/Desktop/microglia/input cxc31/raw_new/"
-#directory = "/Users/sebnemcam/Desktop/Helmholtz/"
-
+'''
+path_seg = "/Users/sebnemcam/Desktop/microglia/input cxc31/gt_new/"
+path_img = "/Users/sebnemcam/Desktop/microglia/input cxc31/raw_new/"
+directory = "/Users/sebnemcam/Desktop/Helmholtz/"
+'''
 seg_list = os.listdir(path_seg)
 img_list = os.listdir(path_img)
 segmentations = []
@@ -148,120 +148,123 @@ model = Unet(
 
 print("Checkpoint 2")
 
-learning_rates = [1e-2] #1e-5,1e-3,1e-4,
+lr = 1e-2
 loss_function = DiceLoss(sigmoid=True)
-#loss_function = torch.nn.CrossEntropyLoss()
 dice_metric = torchmetrics.Dice(zero_division=1).to(device)
-#dice_metric = DiceMetric(include_background=True,ignore_empty=False)
 metric_values = []
 
 epoch_loss_values = []
+lr_values = []
 
-max_epochs = 2000
+max_epochs = 2
 val_interval = 1
 best_metric = -1
-post_pred = (Activations(sigmoid=True))
 
 print("Checkpoint 3")
 
-fig, axs = plt.subplots(1, 2, figsize=(18, 7))
+fig, axs = plt.subplots(3, 1, figsize=(10, 15))
 
-for lr in learning_rates:
-    optimizer = torch.optim.Adam(model.parameters(), lr)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer)
-    for epoch in range(0, max_epochs):
-        print(f"TRAINING WITH LEARNING RATE {lr}")
-        print("-" * 10)
-        print(f"epoch {epoch + 1}/{max_epochs}")
-        model.train()
-        epoch_loss = 0
-        step = 0
-        for batch_data in test_loader:
-            step += 1
-            img, seg = (
-                batch_data['image'].to(device),
-                batch_data['segmentation'].to(device),
-            )
-            # print(f"Input to model: {img.size()}")
-            # print(f"Segmentations: {seg.size()}")
-            optimizer.zero_grad()
-            outputs = model(img)
-            # print(f"Output from model: {outputs.shape}")
-            loss = loss_function(outputs, seg)
-            loss.backward()
-            optimizer.step()
-            scheduler.step(loss.item())
-            epoch_loss += loss.item()
-            print(
-                f"{step}/{len(train_data) // train_loader.batch_size}, "
-                f"train loss: {loss.item():.4f}")
 
-        epoch_loss /= step
-        epoch_loss_values.append(epoch_loss)
-        print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
+optimizer = torch.optim.Adam(model.parameters(), lr)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer)
+for epoch in range(0, max_epochs):
+    print(f"TRAINING WITH LEARNING RATE {lr}")
+    print("-" * 10)
+    print(f"epoch {epoch + 1}/{max_epochs}")
+    model.train()
+    epoch_loss = 0
+    step = 0
+    for batch_data in test_loader:
+        step += 1
+        img, seg = (
+            batch_data['image'].to(device),
+            batch_data['segmentation'].to(device),
+        )
+        # print(f"Input to model: {img.size()}")
+        # print(f"Segmentations: {seg.size()}")
+        optimizer.zero_grad()
+        outputs = model(img)
+        # print(f"Output from model: {outputs.shape}")
+        loss = loss_function(outputs, seg)
+        loss.backward()
+        optimizer.step()
+        scheduler.step(loss.item())
+        epoch_loss += loss.item()
+        lr_values.append(scheduler.get_last_lr())
+        print(
+            f"{step}/{len(train_data) // train_loader.batch_size}, "
+            f"train loss: {loss.item():.4f}")
 
-        if epoch % val_interval == 0:
-            print("Validation")
-            model.eval()
-            batch_idx = 0
-            with torch.no_grad():
-                for val_data in val_loader:
-                    batch_idx += 1
-                    val_img, val_seg = (
-                        val_data['image'].to(device),
-                        val_data['segmentation'].to(device)
-                    )
-                    val_seg = val_seg.type(torch.short)
-                    val_outputs = model(val_img)
-                    val_outputs = [post_pred(i) for i in decollate_batch(val_outputs)]
-                    val_outputs[val_outputs < 0.5] = 0
-                    val_outputs[val_outputs >= 0.5] = 1
-                    dice_metric(preds=val_outputs, target=val_seg)
-                    val_outputs = np.array(val_outputs)
-                    val_outputs_np = val_outputs #.cpu().numpy()
+    epoch_loss /= step
+    epoch_loss_values.append(epoch_loss)
+    print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
 
-                    for i in range(val_outputs_np.shape[0]):
-                        output_image = val_outputs_np[i, 0, :, :, :]
-                        nifti_img = nib.Nifti1Image(output_image, np.eye(4))
-                        output_path = os.path.join(directory,
-                                f"val_outputs_sig/epoch{epoch + 1}_batch{batch_idx}_image{i}.nii.gz")
-                        nib.save(nifti_img, output_path)
-                        print(f"Saved {output_path}")
-                    # print(f"output: {val_outputs}/n label: {val_seg}")
-                    # aggregate the final mean dice result
-                metric = dice_metric.compute().item()
-                # reset the status for next validation round
-                dice_metric.reset()
-                metric_values.append(metric)
-                print(f"Dice Value: {metric}")
+    if epoch % val_interval == 0:
+        print("Validation")
+        model.eval()
+        batch_idx = 0
+        with torch.no_grad():
+            for val_data in val_loader:
+                batch_idx += 1
+                val_img, val_seg = (
+                    val_data['image'].to(device),
+                    val_data['segmentation'].to(device)
+                )
+                val_seg = val_seg.type(torch.short)
+                val_outputs = model(val_img)
+                val_outputs[val_outputs < 0.5] = 0
+                val_outputs[val_outputs >= 0.5] = 1
+                dice_metric(preds=val_outputs, target=val_seg)
+                val_outputs_np = val_outputs.cpu().numpy()
 
-                if metric > best_metric:
-                    best_metric = metric
-                    best_metric_epoch = epoch + 1
-                    #torch.save(model.state_dict(), os.path.join(
-                        #directory, "best_metric_model.pth"))
-                    print("saved new best metric model")
-                    print(
-                        f"current epoch: {epoch + 1} current mean dice: {metric:.4f}"
-                        f"\nbest mean dice: {best_metric:.4f} "
-                        f"at epoch: {best_metric_epoch}"
-                    )
+                '''for i in range(val_outputs_np.shape[0]):
+                    output_image = val_outputs_np[i, 0, :, :, :]
+                    nifti_img = nib.Nifti1Image(output_image, np.eye(4))
+                    output_path = os.path.join(directory,
+                            f"val_outputs_sig/epoch{epoch + 1}_batch{batch_idx}_image{i}.nii.gz")
+                    nib.save(nifti_img, output_path)
+                    print(f"Saved {output_path}")'''
+                # print(f"output: {val_outputs}/n label: {val_seg}")
+
+            # aggregate the final mean dice result
+            metric = dice_metric.compute().item()
+            # reset the status for next validation round
+            dice_metric.reset()
+            metric_values.append(metric)
+            print(f"Dice Value: {metric}")
+
+            if metric > best_metric:
+                best_metric = metric
+                best_metric_epoch = epoch + 1
+                #torch.save(model.state_dict(), os.path.join(
+                    #directory, "best_metric_model.pth"))
+                #print("saved new best metric model")
+                print(
+                    f"current epoch: {epoch + 1} current mean dice: {metric:.4f}"
+                    f"\nbest mean dice: {best_metric:.4f} "
+                    f"at epoch: {best_metric_epoch}"
+                )
 
     # Plotting the loss and dice scores
-    axs[0].plot(range(1, len(epoch_loss_values) + 1), epoch_loss_values, label=f'LR={lr}')
-    axs[1].plot(range(1, len(metric_values) + 1), metric_values, label=f'LR={lr}')
-    epoch_loss_values = []
-    metric_values = []
+axs[0].plot(range(1, max_epochs + 1), epoch_loss_values)
+axs[1].plot(range(1, max_epochs + 1), metric_values)
+axs[2].plot(range(1, len(lr_values) + 1), lr_values)
+
 
 axs[0].set_xlabel('Epochs')
 axs[0].set_ylabel('Loss')
 axs[0].set_title('Dice Loss for Different Learning Rates')
-axs[0].legend()
+axs[0].set_ylim(0,1)
 
 axs[1].set_xlabel('Epochs')
 axs[1].set_ylabel('Dice Score')
 axs[1].set_title('Dice Score for Different Learning Rates')
-axs[1].legend()
+axs[1].set_ylim(0,1)
+
+axs[2].set_xlabel('Epochs')
+axs[2].set_ylabel('Learning Rate')
+axs[2].set_title('Learning Rate Schedule')
+axs[2].set_ylim(0,lr+0.005)
 
 plt.show()
 plt.savefig("/lustre/groups/iterm/sebnem/LearningCurves_no_sig.png")
