d1cc95f52acc6d166d47b2fea686af88aa5c0f32
Şebnem Çam <sebnemacam@gmail.com>
2024-06-30 18:53:19 +0200

    5FCV

--- dataloader.py
+++ dataloader.py
@@ -3,9 +3,6 @@ import os
 import time
 
 import pandas as pd
-from monai.inferers import sliding_window_inference
-from tqdm import tqdm
-from zipfile import ZipFile
 
 import numpy as np
 from sklearn.model_selection import KFold
@@ -14,15 +11,12 @@ import nibabel as nib
 import torch
 import torchmetrics
 
-import monai
-from monai.utils import first
-from monai.data import ArrayDataset, DataLoader, partition_dataset, decollate_batch, Dataset, CacheDataset
+from monai.data import ArrayDataset, DataLoader, CacheDataset
 from monai.networks.nets import Unet
 from monai.networks.layers import Norm
 from monai.losses import DiceLoss, DiceCELoss
 from monai.metrics import DiceMetric
-from monai.transforms import EnsureChannelFirst, Compose, RandRotate90, Resize, ScaleIntensity, LoadImage, LoadImaged, \
-    Resized, ToTensord, RandFlipd, AsDiscrete, Activations, Activationsd
+from monai.transforms import Compose, LoadImaged, Resized, ToTensord, RandFlipd
 
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
@@ -30,7 +24,7 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 path_seg = "/lustre/groups/iterm/Annotated_Datasets/Annotated Datasets/Microglia - Microglia LSM and Confocal/input cxc31/gt_new"
 path_img = "/lustre/groups/iterm/Annotated_Datasets/Annotated Datasets/Microglia - Microglia LSM and Confocal/input cxc31/raw_new"
-directory= "/lustre/groups/iterm/sebnem/"
+directory= "/lustre/groups/iterm/sebnem/runs/30.06_18:50/"
 '''
 path_seg = "/Users/sebnemcam/Desktop/microglia/input cxc31/gt_new/"
 path_img = "/Users/sebnemcam/Desktop/microglia/input cxc31/raw_new/"
@@ -58,9 +52,9 @@ zipped_data = list(zip(images,segmentations))
 #print(f"Zip length: {len(zipped_data)}")
 
 #split into train, test & validation files
-ratio_a = len(zipped_data)*0.8
-train_files_a = [zipped_data[i] for i in range(int(ratio_a))]
-test_files = [zipped_data[i] for i in range(int(ratio_a),len(zipped_data))]
+#ratio_a = len(zipped_data)*0.8
+#train_files_a = [zipped_data[i] for i in range(int(ratio_a))]
+#test_files = [zipped_data[i] for i in range(int(ratio_a),len(zipped_data))]
 
 '''
 ratio_b = len(train_files_a)*0.8
@@ -71,8 +65,7 @@ train_data = [{'image' : img, 'segmentation' : seg } for img,seg in train_files]
 val_data = [{'image' : img, 'segmentation' : seg } for img,seg in val_files]
 '''
 kfold = KFold(5,True,1)
-test_data = [{'image' : img, 'segmentation' : seg } for img,seg in test_files]
-data = [{'image' : img, 'segmentation' : seg } for img,seg in train_files_a]
+data = [{'image' : img, 'segmentation' : seg} for img,seg in zipped_data]
 
 '''
 print(f"Len Train Data: {len(train_data)}")
@@ -102,7 +95,7 @@ dic_transforms = Compose(
     ]
 )
 
-test_set = CacheDataset(test_files,dic_transforms)
+#test_set = CacheDataset(test_files,dic_transforms)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -124,144 +117,163 @@ scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer,thres
 
 metric_values = []
 epoch_loss_values = []
+best_metric_epoch = []
 lr_values = []
 best_metric = []
-max_epochs = 3000
+max_epochs = 1500
 val_interval = 1
 fold = -1
+test_fold = -1
+model_path = ""
 
 fig, axs = plt.subplots(5, 3, figsize=(15, 10))
 
-for train_data, val_data in kfold.split(data['image'],data['segmentation']):
+for train_val_data, test_data in kfold.split(data['image'],data['segmentation']):
 
-    train_set = CacheDataset(train_data,dic_transforms_train)
-    val_set = CacheDataset(val_data,dic_transforms_train)
+    test_set = CacheDataset(test_data, dic_transforms)
+    test_fold += 1
 
-    train_loader = DataLoader(train_set,batch_size=5)
-    test_loader = DataLoader(test_set,batch_size=5)
-    val_loader = DataLoader(val_set,batch_size=5)
+    for train_data, val_data in kfold.split(train_val_data['image'],data['segmentation']):
 
-    fold += 1
+        train_set = CacheDataset(train_data,dic_transforms_train)
+        val_set = CacheDataset(val_data,dic_transforms_train)
 
-    for epoch in range(0, max_epochs):
-        print(f"TRAINING WITH LEARNING RATE {lr}")
-        print("-" * 10)
-        print(f"epoch {epoch + 1}/{max_epochs}")
-        model.train()
-        epoch_loss = 0
-        step = 0
-        for batch_data in test_loader:
-            step += 1
-            img, seg = (
-                batch_data['image'].to(device),
-                batch_data['segmentation'].to(device),
+        train_loader = DataLoader(train_set,batch_size=5)
+        test_loader = DataLoader(test_set,batch_size=5)
+        val_loader = DataLoader(val_set,batch_size=5)
+
+        fold += 1
+
+        for epoch in range(0, max_epochs):
+            print(f"TRAINING WITH LEARNING RATE {lr}")
+            print("-" * 10)
+            print(f"epoch {epoch + 1}/{max_epochs}")
+            model.train()
+            epoch_loss = 0
+            step = 0
+            for batch_data in test_loader:
+                step += 1
+                img, seg = (
+                    batch_data['image'].to(device),
+                    batch_data['segmentation'].to(device),
+                )
+                # print(f"Input to model: {img.size()}")
+                # print(f"Segmentations: {seg.size()}")
+                optimizer.zero_grad()
+                outputs = model(img)
+                # print(f"Output from model: {outputs.shape}")
+                loss = loss_function(outputs, seg)
+                loss.backward()
+                optimizer.step()
+                scheduler.step(loss.item())
+                epoch_loss += loss.item()
+                lr_values[fold].append(scheduler.get_last_lr())
+                print(
+                    f"{step}/{len(train_data) // train_loader.batch_size}, "
+                    f"train loss: {loss.item():.4f}")
+
+            epoch_loss /= step
+            epoch_loss_values[fold].append(epoch_loss)
+            print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
+
+            if epoch % val_interval == 0:
+                print("Validation")
+                model.eval()
+                batch_idx = 0
+                with torch.no_grad():
+                    for val_data in val_loader:
+                        batch_idx += 1
+                        val_img, val_seg = (
+                            val_data['image'].to(device),
+                            val_data['segmentation'].to(device)
+                        )
+                        val_seg = val_seg.type(torch.short)
+                        val_outputs = model(val_img)
+                        val_outputs[val_outputs < 0.5] = 0
+                        val_outputs[val_outputs >= 0.5] = 1
+                        dice_metric(preds=val_outputs, target=val_seg)
+                        #val_outputs_np = val_outputs.cpu().numpy()
+                        #val_seg_np = val_seg.cpu().numpy()
+                        #val_img_np = val_img.cpu().numpy()
+
+                        '''
+                        for i in range(val_outputs_np.shape[0]):
+                            # Extract the ith sample, first channel, all depth, height, and width slices
+                            output_image = val_outputs_np[i, 0, :, :, :]
+    
+                            # Create NIfTI images
+                            output_nifti = nib.Nifti1Image(output_image, np.eye(4))
+    
+                            #define file names
+                            output_filename = f"outputs/output_epoch{epoch + 1}_batch{batch_idx}_image{i}.nii.gz"
+    
+                            #save NIFTI images to temporary files
+                            output_filepath = os.path.join(directory, output_filename)
+                            nib.save(output_nifti, output_filepath)
+                           # print(f"Saved pred, seg & raw")
+                           '''
+
+                    # aggregate the final mean dice result
+                    metric = dice_metric.compute().item()
+                    # reset the status for next validation round
+                    dice_metric.reset()
+                    metric_values[fold].append(metric)
+                    print(f"Dice Value: {metric}")
+
+                    if metric > best_metric[fold]:
+                        best_metric[fold] = metric
+                        best_metric_epoch[fold] = epoch +1
+                        print(f"Fold {fold} \nBest Dice {best_metric[fold]} \nat epoch{best_metric_epoch[fold]}")
+                        model_path = os.path.join(directory, "best_metric_model.pth")
+                        torch.save(model.state_dict(),model_path)
+
+        # Plotting the loss and dice scores
+        colors = plt.cm.viridis(np.linspace(0, 1, 5))
+
+        axs[0].plot(range(1, max_epochs + 1), epoch_loss_values[fold], label=f'Fold {fold+1}', color=colors[fold])
+        axs[1].plot(range(1, max_epochs + 1), metric_values[fold], label=f'Fold {fold+1}', color=colors[fold])
+        axs[2].plot(range(1, len(lr_values) + 1), lr_values[fold], label=f'Fold {fold+1}', color=colors[fold])
+
+        axs[0].set_xlabel('Epochs')
+        axs[0].set_ylabel('Loss')
+        axs[0].set_title('Dice Loss for Different Folds')
+        axs[0].set_ylim(0,1)
+
+        axs[1].set_xlabel('Epochs')
+        axs[1].set_ylabel('Dice Score')
+        axs[1].set_title('Dice Score for Different Folds')
+        axs[1].set_ylim(0,1)
+
+        axs[2].set_xlabel('Epochs')
+        axs[2].set_ylabel('Learning Rate')
+        axs[2].set_title('Learning Rate Schedule for Different Folds')
+        axs[2].set_ylim(0,lr+0.005)
+
+        for ax in axs:
+            ax.legend()
+
+
+    plt.show()
+    plt.savefig(f"/lustre/groups/iterm/sebnem/LearningCurvesTestFold{test_fold}.png")
+
+    model = Unet()
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+    with torch.no_grad():
+        for test_data in test_loader:
+            img,seg = (
+                test_data['image'].to(device),
+                test_data['segmentation'].to(device)
             )
-            # print(f"Input to model: {img.size()}")
-            # print(f"Segmentations: {seg.size()}")
-            optimizer.zero_grad()
-            outputs = model(img)
-            # print(f"Output from model: {outputs.shape}")
-            loss = loss_function(outputs, seg)
-            loss.backward()
-            optimizer.step()
-            scheduler.step(loss.item())
-            epoch_loss += loss.item()
-            lr_values[fold].append(scheduler.get_last_lr())
-            print(
-                f"{step}/{len(train_data) // train_loader.batch_size}, "
-                f"train loss: {loss.item():.4f}")
-
-        epoch_loss /= step
-        epoch_loss_values[fold].append(epoch_loss)
-        print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
-
-        if epoch % val_interval == 0:
-            print("Validation")
-            model.eval()
-            batch_idx = 0
-            with torch.no_grad():
-                for val_data in val_loader:
-                    batch_idx += 1
-                    val_img, val_seg = (
-                        val_data['image'].to(device),
-                        val_data['segmentation'].to(device)
-                    )
-                    val_seg = val_seg.type(torch.short)
-                    val_outputs = model(val_img)
-                    val_outputs[val_outputs < 0.5] = 0
-                    val_outputs[val_outputs >= 0.5] = 1
-                    dice_metric(preds=val_outputs, target=val_seg)
-                    #val_outputs_np = val_outputs.cpu().numpy()
-                    #val_seg_np = val_seg.cpu().numpy()
-                    #val_img_np = val_img.cpu().numpy()
-
-                    '''
-                    for i in range(val_outputs_np.shape[0]):
-                        # Extract the ith sample, first channel, all depth, height, and width slices
-                        output_image = val_outputs_np[i, 0, :, :, :]
-                        seg_image = val_seg_np[i, 0, :, :, :]
-                        raw_image = val_img_np[i, 0, :, :, :]
-
-                        # Create NIfTI images
-                        output_nifti = nib.Nifti1Image(output_image, np.eye(4))
-                        seg_nifti = nib.Nifti1Image(seg_image, np.eye(4))
-                        raw_nifti = nib.Nifti1Image(raw_image, np.eye(4))
-
-                        #define file names
-                        output_filename = f"outputs/output_epoch{epoch + 1}_batch{batch_idx}_image{i}.nii.gz"
-                        seg_filename = f"gts/seg_epoch{epoch + 1}_batch{batch_idx}_image{i}.nii.gz"
-                        raw_filename = f"raws/raw_epoch{epoch + 1}_batch{batch_idx}_image{i}.nii.gz"
-
-                        #save NIFTI images to temporary files
-                        output_filepath = os.path.join(directory, output_filename)
-                        seg_filepath = os.path.join(directory, seg_filename)
-                        raw_filepath = os.path.join(directory, raw_filename)
-                        nib.save(output_nifti, output_filepath)
-                        nib.save(seg_nifti, seg_filepath)
-                        nib.save(raw_nifti, raw_filepath)
-                       # print(f"Saved pred, seg & raw")
-                       '''
-
-                # aggregate the final mean dice result
-                metric = dice_metric.compute().item()
-                # reset the status for next validation round
-                dice_metric.reset()
-                metric_values[fold].append(metric)
-                print(f"Dice Value: {metric}")
-
-                if metric > best_metric[fold]:
-                    best_metric[fold] = metric
+            seg = seg.type(torch.short)
+            outputs = model(val_img)
+            outputs[outputs < 0.5] = 0
+            outputs[outputs >= 0.5] = 1
+            dice_metric(preds=outputs, target=seg)
+            metric = dice_metric.compute().item()
+            dice_metric.reset()
+            print(f"Test Dice Value: {metric}")
 
 
-    # Plotting the loss and dice scores
-    colors = plt.cm.viridis(np.linspace(0, 1, 5))
-
-    axs[0].plot(range(1, max_epochs + 1), epoch_loss_values[fold], label=f'Fold {fold+1}', color=colors[fold])
-    axs[1].plot(range(1, max_epochs + 1), metric_values[fold], label=f'Fold {fold+1}', color=colors[fold])
-    axs[2].plot(range(1, len(lr_values) + 1), lr_values[fold], label=f'Fold {fold+1}', color=colors[fold])
-
-    axs[0].set_xlabel('Epochs')
-    axs[0].set_ylabel('Loss')
-    axs[0].set_title('Dice Loss for Different Folds')
-    axs[0].set_ylim(0,1)
-
-    axs[1].set_xlabel('Epochs')
-    axs[1].set_ylabel('Dice Score')
-    axs[1].set_title('Dice Score for Different Folds')
-    axs[1].set_ylim(0,1)
-
-    axs[2].set_xlabel('Epochs')
-    axs[2].set_ylabel('Learning Rate')
-    axs[2].set_title('Learning Rate Schedule for Different Folds')
-    axs[2].set_ylim(0,lr+0.005)
-
-    for ax in axs:
-        ax.legend()
-
-
-plt.show()
-plt.savefig("/lustre/groups/iterm/sebnem/LearningCurves.png")
-
-df = {'Fold' : [0,1,2,3,4], 'Best Dice': best_metric}
-#df.to_csv(os.path.join(directory,'folds'))
-#plt.savefig("/Users/sebnemcam/Desktop/LearningCurves.png")
+df = {'Fold' : [0,1,2,3,4], 'Best Dice': best_metric, 'Best Dice Epoch':best_metric_epoch}
+df.to_csv(os.path.join(directory,'folds'))
