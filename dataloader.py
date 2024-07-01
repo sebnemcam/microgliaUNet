bd0d5aff9778eb826d1678003337f0846e2d584a
Şebnem Çam <sebnemacam@gmail.com>
2024-07-01 09:51:57 +0200

    fixed CV

--- dataloader.py
+++ dataloader.py
@@ -24,16 +24,19 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 path_seg = "/lustre/groups/iterm/Annotated_Datasets/Annotated Datasets/Microglia - Microglia LSM and Confocal/input cxc31/gt_new"
 path_img = "/lustre/groups/iterm/Annotated_Datasets/Annotated Datasets/Microglia - Microglia LSM and Confocal/input cxc31/raw_new"
-directory= "/lustre/groups/iterm/sebnem/runs/30.06_18:50/"
+directory= "/lustre/groups/iterm/sebnem/runs/01.07_9:41/"
 '''
 path_seg = "/Users/sebnemcam/Desktop/microglia/input cxc31/gt_new/"
 path_img = "/Users/sebnemcam/Desktop/microglia/input cxc31/raw_new/"
 directory = "/Users/sebnemcam/Desktop/Helmholtz/"
 '''
+
 seg_list = os.listdir(path_seg)
 img_list = os.listdir(path_img)
 segmentations = []
 images = []
+
+data = []
 
 print("I am inside the python file")
 
@@ -45,10 +48,10 @@ for i in range(len(seg_list)):
 
         imgFile = os.path.join(path_img, img_list[i])
         images.append(imgFile)
-
+        data.append([{'image' : imgFile, 'segmentation' : segFile, 'name': img_list[i]}])
 
 # zip data into one list of image/segmenttaion pairs
-zipped_data = list(zip(images,segmentations))
+#zipped_data = list(zip(images,segmentations))
 #print(f"Zip length: {len(zipped_data)}")
 
 #split into train, test & validation files
@@ -64,8 +67,9 @@ val_files = [train_files_a[i] for i in range(int(ratio_b),len(train_files_a))]
 train_data = [{'image' : img, 'segmentation' : seg } for img,seg in train_files]
 val_data = [{'image' : img, 'segmentation' : seg } for img,seg in val_files]
 '''
-kfold = KFold(5,True,1)
-data = [{'image' : img, 'segmentation' : seg} for img,seg in zipped_data]
+kfold = KFold(n_splits=5,shuffle=True,random_state=1)
+#data = [{'image' : img, 'segmentation' : seg} for img,seg in zipped_data]
+#print(f"images: {data[0]}")
 
 '''
 print(f"Len Train Data: {len(train_data)}")
@@ -89,8 +93,8 @@ dic_transforms_train = Compose(
 
 dic_transforms = Compose(
     [
-        LoadImaged(keys, ensure_channel_first=True, image_only=True),
-        Resized(keys,spatial_size=(128, 128, 128)),
+        LoadImaged(keys=keys, ensure_channel_first=True, image_only=True),
+        Resized(keys=keys,spatial_size=(128, 128, 128)),
         ToTensord(keys)
     ]
 )
@@ -115,25 +119,32 @@ dice_metric = torchmetrics.Dice(zero_division=1).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer,threshold=0.00001, patience=50)
 
-metric_values = []
-epoch_loss_values = []
-best_metric_epoch = []
-lr_values = []
-best_metric = []
 max_epochs = 1500
+metric_values = np.empty(5,5,max_epochs)
+epoch_loss_values = np.empty(5,5,max_epochs)
+best_metric_epoch = np.empty(5,5)
+lr_values = np.ones(5,5,1000)
+best_metric = np.empty(5,5)
 val_interval = 1
 fold = -1
 test_fold = -1
 model_path = ""
 
-fig, axs = plt.subplots(5, 3, figsize=(15, 10))
+for i, (train_val_idx, test_idx) in enumerate(kfold.split(data)):
 
-for train_val_data, test_data in kfold.split(data['image'],data['segmentation']):
+    fig, axs = plt.subplots(5, 3, figsize=(15, 10))
 
+    train_val_data = [data[i] for i in train_val_idx]
+
+    test_data = [data[i] for i in test_idx]
     test_set = CacheDataset(test_data, dic_transforms)
+
     test_fold += 1
 
-    for train_data, val_data in kfold.split(train_val_data['image'],data['segmentation']):
+    for i, (train_idx, val_idx) in enumerate(kfold.split(train_val_data)):
+
+        train_data = [train_val_data[i] for i in train_idx]
+        val_data = [train_val_data[i] for i in val_idx]
 
         train_set = CacheDataset(train_data,dic_transforms_train)
         val_set = CacheDataset(val_data,dic_transforms_train)
@@ -153,12 +164,14 @@ for train_val_data, test_data in kfold.split(data['image'],data['segmentation'])
             step = 0
             for batch_data in test_loader:
                 step += 1
-                img, seg = (
+                img, seg, name = (
                     batch_data['image'].to(device),
                     batch_data['segmentation'].to(device),
+                    batch_data['name']
                 )
                 # print(f"Input to model: {img.size()}")
                 # print(f"Segmentations: {seg.size()}")
+                print(f"name: {name}")
                 optimizer.zero_grad()
                 outputs = model(img)
                 # print(f"Output from model: {outputs.shape}")
@@ -167,13 +180,13 @@ for train_val_data, test_data in kfold.split(data['image'],data['segmentation'])
                 optimizer.step()
                 scheduler.step(loss.item())
                 epoch_loss += loss.item()
-                lr_values[fold].append(scheduler.get_last_lr())
+                lr_values[test_fold][fold].append(scheduler.get_last_lr())
                 print(
                     f"{step}/{len(train_data) // train_loader.batch_size}, "
                     f"train loss: {loss.item():.4f}")
 
             epoch_loss /= step
-            epoch_loss_values[fold].append(epoch_loss)
+            epoch_loss_values[test_fold][fold].append(epoch_loss)
             print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
 
             if epoch % val_interval == 0:
@@ -183,9 +196,10 @@ for train_val_data, test_data in kfold.split(data['image'],data['segmentation'])
                 with torch.no_grad():
                     for val_data in val_loader:
                         batch_idx += 1
-                        val_img, val_seg = (
+                        val_img, val_seg, name = (
                             val_data['image'].to(device),
-                            val_data['segmentation'].to(device)
+                            val_data['segmentation'].to(device),
+                            val_data['name']
                         )
                         val_seg = val_seg.type(torch.short)
                         val_outputs = model(val_img)
@@ -193,8 +207,6 @@ for train_val_data, test_data in kfold.split(data['image'],data['segmentation'])
                         val_outputs[val_outputs >= 0.5] = 1
                         dice_metric(preds=val_outputs, target=val_seg)
                         #val_outputs_np = val_outputs.cpu().numpy()
-                        #val_seg_np = val_seg.cpu().numpy()
-                        #val_img_np = val_img.cpu().numpy()
 
                         '''
                         for i in range(val_outputs_np.shape[0]):
@@ -205,25 +217,25 @@ for train_val_data, test_data in kfold.split(data['image'],data['segmentation'])
                             output_nifti = nib.Nifti1Image(output_image, np.eye(4))
     
                             #define file names
-                            output_filename = f"outputs/output_epoch{epoch + 1}_batch{batch_idx}_image{i}.nii.gz"
+                            output_filename = f"outputs/{name}_epoch{epoch + 1}_"
     
                             #save NIFTI images to temporary files
                             output_filepath = os.path.join(directory, output_filename)
                             nib.save(output_nifti, output_filepath)
-                           # print(f"Saved pred, seg & raw")
+                           # print(f"Saved")
                            '''
 
                     # aggregate the final mean dice result
                     metric = dice_metric.compute().item()
                     # reset the status for next validation round
                     dice_metric.reset()
-                    metric_values[fold].append(metric)
+                    metric_values[test_fold][fold].append(metric)
                     print(f"Dice Value: {metric}")
 
-                    if metric > best_metric[fold]:
-                        best_metric[fold] = metric
-                        best_metric_epoch[fold] = epoch +1
-                        print(f"Fold {fold} \nBest Dice {best_metric[fold]} \nat epoch{best_metric_epoch[fold]}")
+                    if metric > best_metric[test_fold][fold]:
+                        best_metric[test_fold][fold] = metric
+                        best_metric_epoch[test_fold][fold] = epoch +1
+                        print(f"Fold {fold} \nBest Dice {best_metric[test_fold][fold]} \nat epoch{best_metric_epoch[test_fold][fold]}")
                         model_path = os.path.join(directory, "best_metric_model.pth")
                         torch.save(model.state_dict(),model_path)
 
@@ -252,9 +264,9 @@ for train_val_data, test_data in kfold.split(data['image'],data['segmentation'])
         for ax in axs:
             ax.legend()
 
-
+    plt.title(f"Test Fold {test_fold}")
     plt.show()
-    plt.savefig(f"/lustre/groups/iterm/sebnem/LearningCurvesTestFold{test_fold}.png")
+    plt.savefig(f"/lustre/groups/iterm/sebnem/runs/01.07_9:41/test_fold{test_fold}/LearningCurvesTestFold{test_fold}.png")
 
     model = Unet()
     model.load_state_dict(torch.load(model_path))
@@ -273,6 +285,23 @@ for train_val_data, test_data in kfold.split(data['image'],data['segmentation'])
             metric = dice_metric.compute().item()
             dice_metric.reset()
             print(f"Test Dice Value: {metric}")
+
+            outputs_np = outputs.cpu().numpy()
+
+            for i in range(outputs_np.shape[0]):
+                # Extract the ith sample, first channel, all depth, height, and width slices
+                output_image = outputs_np[i, 0, :, :, :]
+
+                # Create NIfTI images
+                output_nifti = nib.Nifti1Image(output_image, np.eye(4))
+
+                # define file names
+                output_filename = f"test_fold{test_fold}/outputs/{name}"
+
+                # save NIFTI images to temporary files
+                output_filepath = os.path.join(directory, output_filename)
+                nib.save(output_nifti, output_filepath)
+                print(f"Saved")
 
 
 df = {'Fold' : [0,1,2,3,4], 'Best Dice': best_metric, 'Best Dice Epoch':best_metric_epoch}
